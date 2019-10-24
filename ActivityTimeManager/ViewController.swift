a46645c807eb28489648e3fb1a70439e6a1ff0a6
Alex Cho <alexjscho21@gmail.com>
2019-10-24 16:41:09 -0700

    Initial Commit

---
+++ ActivityTimeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityTimeManager
+//
+//  Created by Alex Cho on 2019-10-24.
+//  Copyright © 2019 Alex Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
